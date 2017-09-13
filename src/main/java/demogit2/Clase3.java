b08e2f218594b763314cac5770f8d01f0d804f85
Juan Pablo Jimenez <jchantej@gmail.com>
2017-09-14 00:33:01 +0200

    cambios en la clase 3

--- src/main/java/demogit2/Clase3.java
+++ src/main/java/demogit2/Clase3.java
@@ -2,7 +2,25 @@ package demogit2;
 
 public class Clase3 {
 
-    public void m() {
+	private int int_valor;
+	
+
+	/**
+	 * @return the int_valor
+	 */
+	private int getInt_valor() {
+		return int_valor;
+	}
+
+	/**
+	 * @param int_valor the int_valor to set
+	 */
+	private void setInt_valor(int int_valor) {
+		this.int_valor = int_valor;
+	}
+	
+    public void m() {	
         
     }
+
 }
